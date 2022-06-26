c430f2faf581e808c052d95161aa42de00e13b5c
Alice Cieslik <alice.cieslik@gmail.com>
2022-06-26 10:38:15 +0200

    add basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
+import "./components/ColorCard.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1>Color Saver</h1>
+      <div className="App">
+        <div className="colorCard">
+          <div className="colorCard_change">Color Change</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
